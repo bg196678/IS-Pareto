51007b4fbbcf02f747b9a9506218755efe54cd50
Lennart <lennart.thiele@rwth-aachen.de>
2025-06-16 13:59:40 +0200

    Example WIP

--- examples/system_1/system_1.py
+++ examples/system_1/system_1.py
@@ -1,5 +1,18 @@
 from pathlib import Path
-from insiliciopt.species import Species, TransitionState
+
+from insiliciopt.species import (
+    Species,
+    TransitionState,
+    Reaction,
+)
+from insiliciopt.kinetics import Kinetics
+from insiliciopt.reactor import Reactor
+from insiliciopt.solvation import Solvation
+from insiliciopt.optimizer import (
+    TSEmoOptimizer,
+    OptimizationSpecies,
+    OptimizationBoundaries,
+)
 
 ##############
 # Paths
@@ -38,7 +51,93 @@ ITS2 = Species(
     name="ITS2",
     mass=0.159 + 0.071,
     fchk_file_path=gaussian_dir / "ITS2.fchk",
-    energy=-847.734323929
+    energy=-847.734323929,
 )
 
 # TODO
+
+product_1 = None
+product_2 = None
+product_3 = None
+
+
+##################
+# Transition State
+##################
+
+TS1_fwd = TransitionState(
+    name="TS1_fwd",
+    fchk_file_path=gaussian_dir / "TS1_fwd.fchk",
+    energy=-847.759104357,
+)
+
+# TODO
+
+
+##############
+# Reactions
+##############
+
+reaction_1_fwd = Reaction(
+    name="R1_fwd",
+    transition_state=TS1_fwd,
+)
+ # TODO
+
+reactions = [
+    reaction_1_fwd,
+]
+
+############
+# Kinetics
+############
+
+kinetics = Kinetics(
+    reactions=reactions,
+    tunneling_correction="eckart",
+)
+
+############
+# Solvation
+############
+
+solvation = Solvation(
+    reactions=reactions,
+)
+
+############
+# Reactor
+############
+
+reactor = Reactor(
+    reactions=reactions,
+    kinetics=kinetics,
+    solvation=solvation,
+)
+
+
+############
+# Optimizer
+############
+optimzation_species = OptimizationSpecies(
+    reactant_1=substrate,
+    reactant_2=nucleophilic,
+    products=[
+        product_1,
+        product_2,
+        product_3,
+    ]
+)
+optimization_boundaries = OptimizationBoundaries(
+    temperature=(60, 140),
+    concentration_reactant_1=(100, 500),
+    concentration_ratio=(10, 5.0),
+    time=(0.5, 2.0),
+)
+optimizer = TSEmoOptimizer(
+    species=optimzation_species,
+    boundaries=optimization_boundaries,
+    reactor=reactor,
+    output_directory=output_dir,
+    num_lhs_points=20,
+)
